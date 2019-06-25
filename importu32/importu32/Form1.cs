f55b2d0db229abdcc495c8898c51d748595510c9
matthewb2 <bestsciencekwabe@gmail.com>
2019-06-26 00:59:12 +0900

    key hook

--- importu32/importu32/Form1.cs
+++ importu32/importu32/Form1.cs
@@ -16,19 +16,132 @@ namespace importu32
 {
     public partial class Form1 : Form
     {
+        
+
         public Form1()
         {
             InitializeComponent();
+
+            LowLevelKeyboardHook kbh = new LowLevelKeyboardHook();
+            kbh.OnKeyPressed += kbh_OnKeyPressed;
+            //kbh.OnKeyUnpressed += kbh_OnKeyUnpressed;
+            kbh.HookKeyboard();
+
         }
 
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        public static extern void SetWindowText(int hWnd, String text);
+        //bool lctrlKeyPressed;
+        //bool f1KeyPressed;
 
+        void kbh_OnKeyPressed(object sender, Keys e)
+        {
+            if (e == Keys.LControlKey)
+            {
+                //lctrlKeyPressed = true;
+            }
+            else if (e == Keys.F1)
+            {
+                //f1KeyPressed = true;
+                MessageBox.Show("F1 pressed");
+            }
+            CheckKeyCombo();
+        }
+
+        void kbh_OnKeyUnPressed(object sender, Keys e)
+        {
+            if (e == Keys.LControlKey)
+            {
+                //lctrlKeyPressed = false;
+            }
+            else if (e == Keys.F1)
+            {
+                //f1KeyPressed = false;
+            }
+        }
+
+        void CheckKeyCombo()
+        {
+            
+        }
 
         private void button1_Click_1(object sender, EventArgs e)
         {
             IntPtr wHnd = this.Handle;//assuming you are in a C# form application
-            SetWindowText(wHnd.ToInt32(), "New Window Title");
+            //SetWindowText(wHnd.ToInt32(), "New Window Title");
+
+
+
+        }
+    }
+
+    public class LowLevelKeyboardHook
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_SYSKEYDOWN = 0x0104;
+        private const int WM_KEYUP = 0x101;
+        private const int WM_SYSKEYUP = 0x105;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        public event EventHandler<Keys> OnKeyPressed;
+        public event EventHandler<Keys> OnKeyUnpressed;
+
+        private LowLevelKeyboardProc _proc;
+        private IntPtr _hookID = IntPtr.Zero;
+
+        public LowLevelKeyboardHook()
+        {
+            _proc = HookCallback;
+        }
+
+        public void HookKeyboard()
+        {
+            _hookID = SetHook(_proc);
+        }
+
+        public void UnHookKeyboard()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (System.Diagnostics.Process curProcess = System.Diagnostics.Process.GetCurrentProcess())
+            using (System.Diagnostics.ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                OnKeyPressed.Invoke(this, ((Keys)vkCode));
+            }
+            else if (nCode >= 0 && wParam == (IntPtr)WM_KEYUP || wParam == (IntPtr)WM_SYSKEYUP)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                OnKeyUnpressed.Invoke(this, ((Keys)vkCode));
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
         }
     }
 }
